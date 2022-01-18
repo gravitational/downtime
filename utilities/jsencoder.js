0bac6138498158290dcbf8af361f3609d4b0fae8
C-STYR <cole@goteleport.com>
2022-01-18 11:16:44 -0900

    site and encoding changes

--- utilities/jsencoder.js
+++ utilities/jsencoder.js
@@ -3,17 +3,3 @@ const string = "Spilled Coffee Remains Nation's Leading Cybersecurity Threat htt
 const encoded = encodeURIComponent(string)
 
 console.log(encoded)
-
-
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-// }
-
-// function getInt() {
-  
-//   return Math.floor(Math.random() * 90000 + 10000); //The maximum is exclusive and the minimum is inclusive
-// }
-
-// console.log(getInt())

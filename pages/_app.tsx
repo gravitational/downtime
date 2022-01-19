fa9879a9ce28856eac7a65697ecc2b4e407a0557
C-STYR <cole@goteleport.com>
2022-01-18 16:23:27 -0900

    flavio solution to ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,14 @@ import Layout from "components/Layout";
 import Head from "components/Head";
 import Script from "next/script";
 import "../styles/globals.css";
-import { GTMPageView } from "utilities/google";
+import { pageview } from "utilities/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: any) => {
-      GTMPageView(url); //log for google tag manager
+      pageview(url); 
     };
     //When the component is mounted, subscribe to router changes
     //and log those page views
@@ -28,34 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <>
-          {/* Google Tag Manager - Global base code */}
-          <Script id="script_gtm">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}
-          </Script>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
-          />
-          <Script id="script_gtag">
-            {`window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', "${process.env.NEXT_PUBLIC_GTAG_ID}");`}
-          </Script>
-        </>
-      </Head>
+      <Head />
       <Layout>
         <Component {...pageProps} />
       </Layout>

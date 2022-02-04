6f3c835289da46d75f559a9c0325211856b93572
Delia Constantino <delia.constantino@goteleport.com>
2022-02-04 13:18:08 -0500

    test mobile without width and height defined

---
+++ TestingResultsMobile.js
@@ -0,0 +1,108 @@
+//Static Mobile, no developer defined width and height
+//Test 1:
+// ➜  downtime git:(delia/test-images) ✗ sh har-stat.sh ./StaticMobile1.har
+// Processing HAR file ./StaticMobile1.har
+// IMAGES:          "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FdowntimeLogo.68858d5e.png&w=3840&q=75 9000"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FmanagerChat%402x.7e01dd4d.jpg&w=3840&q=75 27962"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FTCP-IP-settings%402x.1860b356.jpg&w=3840&q=75 16494"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuptimeSuccessManager%402x.67b00024.jpg&w=3840&q=75 76202"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuniverseDown%402x.9f54812b.jpg&w=3840&q=75 128804"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbotDoesntSpam%402x.4f1888a2.png&w=3840&q=75 41500"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FidentityNotStolen%402x.49d65891.jpg&w=3840&q=75 53514"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foss%402x.f48bf53e.jpg&w=3840&q=75 28586"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkernelmodule%402x.ccf75440.jpg&w=3840&q=75 93526"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcoffee%402x.bd7e8174.jpg&w=3840&q=75 54749"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirewall2%402x.cd48b949.jpg&w=3840&q=75 58294"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fscreaming%402x.3d326115.jpg&w=3840&q=75 63930"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcavepainting%402x.78a9233c.jpg&w=3840&q=75 307391"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fexpert2%402x.91e07a99.jpg&w=3840&q=75 26986"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foilpipe%402x.8003ed5e.jpg&w=3840&q=75 68952"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbored%402x.58453e65.jpg&w=3840&q=75 77755"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fromance%402x.f1fc2faa.jpg&w=3840&q=75 77809"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwalmartCart%402x.16298861.png&w=3840&q=75 26022"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frobinhood%402x.949bd9ef.jpg&w=3840&q=75 61972"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgoodhuman%402x.ead342f6.jpg&w=3840&q=75 65806"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbarnesAndNobleClackamas%402x.a2f30809.jpeg&w=3840&q=75 97377"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnuclear%402x.f4f3a5e2.jpg&w=3840&q=75 14774"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpasswords%402x.db19b20d.jpg&w=3840&q=75 27424"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fczar%402x.72442611.jpg&w=3840&q=75 41623"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmicrosoft%402x.e466e7bb.jpg&w=3840&q=75 137890"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcleanblinds%402x.2a8f4780.png&w=1920&q=75 34094"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3monitors%402x.dcb3adc9.jpg&w=3840&q=75 10180"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkidhacker%402x.4b2e55ca.jpg&w=3840&q=75 57416"
+// On content load:          9379.500000010012
+// On load:                  10847.875000006752
+// Number of requests:       28
+// Total time with queueing: 137090.75000000303
+// Requests time:            59129.25000008452
+// Bytes:                    1786032
+
+//Test 2:
+// ➜  downtime git:(delia/test-images) ✗ sh har-stat.sh ./StaticMobile2.har
+// Processing HAR file ./StaticMobile2.har
+// IMAGES:          "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuptimeSuccessManager%402x.67b00024.jpg&w=3840&q=75 76202"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuniverseDown%402x.9f54812b.jpg&w=3840&q=75 128804"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbotDoesntSpam%402x.4f1888a2.png&w=3840&q=75 41500"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FidentityNotStolen%402x.49d65891.jpg&w=3840&q=75 53514"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foss%402x.f48bf53e.jpg&w=3840&q=75 28586"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkernelmodule%402x.ccf75440.jpg&w=3840&q=75 93526"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcoffee%402x.bd7e8174.jpg&w=3840&q=75 54749"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirewall2%402x.cd48b949.jpg&w=3840&q=75 58294"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fscreaming%402x.3d326115.jpg&w=3840&q=75 63930"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcavepainting%402x.78a9233c.jpg&w=3840&q=75 307391"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fexpert2%402x.91e07a99.jpg&w=3840&q=75 26986"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foilpipe%402x.8003ed5e.jpg&w=3840&q=75 68952"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbored%402x.58453e65.jpg&w=3840&q=75 77755"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fromance%402x.f1fc2faa.jpg&w=3840&q=75 77809"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwalmartCart%402x.16298861.png&w=3840&q=75 26022"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frobinhood%402x.949bd9ef.jpg&w=3840&q=75 61972"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgoodhuman%402x.ead342f6.jpg&w=3840&q=75 65806"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbarnesAndNobleClackamas%402x.a2f30809.jpeg&w=3840&q=75 97377"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnuclear%402x.f4f3a5e2.jpg&w=3840&q=75 14774"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpasswords%402x.db19b20d.jpg&w=3840&q=75 27424"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fczar%402x.72442611.jpg&w=3840&q=75 41623"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmicrosoft%402x.e466e7bb.jpg&w=3840&q=75 137890"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcleanblinds%402x.2a8f4780.png&w=1920&q=75 34094"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3monitors%402x.dcb3adc9.jpg&w=3840&q=75 10180"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkidhacker%402x.4b2e55ca.jpg&w=3840&q=75 57416"
+// On content load:          null
+// On load:                  null
+// Number of requests:       25
+// Total time with queueing: 135951.62500004517
+// Requests time:            56473.12499995623
+// Bytes:                    1732576
+
+//Test 3:
+// ➜  downtime git:(delia/test-images) ✗ sh har-stat.sh ./StaticMobile3.har
+// Processing HAR file ./StaticMobile3.har
+// IMAGES:          "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuptimeSuccessManager%402x.67b00024.jpg&w=3840&q=75 76202"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FuniverseDown%402x.9f54812b.jpg&w=3840&q=75 128804"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbotDoesntSpam%402x.4f1888a2.png&w=3840&q=75 41500"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FidentityNotStolen%402x.49d65891.jpg&w=3840&q=75 53514"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foss%402x.f48bf53e.jpg&w=3840&q=75 28586"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkernelmodule%402x.ccf75440.jpg&w=3840&q=75 93526"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcoffee%402x.bd7e8174.jpg&w=3840&q=75 54749"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirewall2%402x.cd48b949.jpg&w=3840&q=75 58294"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fscreaming%402x.3d326115.jpg&w=3840&q=75 63930"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcavepainting%402x.78a9233c.jpg&w=3840&q=75 307391"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fexpert2%402x.91e07a99.jpg&w=3840&q=75 26986"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Foilpipe%402x.8003ed5e.jpg&w=3840&q=75 68952"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbored%402x.58453e65.jpg&w=3840&q=75 77755"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fromance%402x.f1fc2faa.jpg&w=3840&q=75 77809"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FwalmartCart%402x.16298861.png&w=3840&q=75 26022"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frobinhood%402x.949bd9ef.jpg&w=3840&q=75 61972"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgoodhuman%402x.ead342f6.jpg&w=3840&q=75 65806"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FbarnesAndNobleClackamas%402x.a2f30809.jpeg&w=3840&q=75 97377"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnuclear%402x.f4f3a5e2.jpg&w=3840&q=75 14774"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpasswords%402x.db19b20d.jpg&w=3840&q=75 27424"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fczar%402x.72442611.jpg&w=3840&q=75 41623"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmicrosoft%402x.e466e7bb.jpg&w=3840&q=75 137890"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcleanblinds%402x.2a8f4780.png&w=1920&q=75 34094"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3monitors%402x.dcb3adc9.jpg&w=3840&q=75 10180"
+// "http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkidhacker%402x.4b2e55ca.jpg&w=3840&q=75 57416"
+// On content load:          null
+// On load:                  null
+// Number of requests:       25
+// Total time with queueing: 139725.75000004144
+// Requests time:            56670.50000006566
+// Bytes:                    1732576

82141ff722495e317e10244221303c847328bfc6
Delia Constantino <delia.constantino@goteleport.com>
2022-02-09 14:58:40 -0500

    add gtm

--- utilities/google/gtag.ts
+++ utilities/google/gtag.ts
@@ -1,8 +1,8 @@
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url: URL) => {
+export const GTAGPageView = (url: URL) => {
   //"as string" added per https://stackoverflow.com/questions/68136888/types-gtag-js-error-argument-of-type-config-is-not-assignable-to-parameter
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string, {
+  window.gtag('config', process.env.NEXT_PUBLIC_GTAG_ID as string, {
     page_path: url,
   })
 }

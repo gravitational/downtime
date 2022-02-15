20a1e0f97bcf0e6718747c8cafd1ae00690fe167
Delia Constantino <delia.constantino@goteleport.com>
2022-02-14 20:15:30 -0500

    remove unused code

--- scripts/gen-rss.ts
+++ scripts/gen-rss.ts
@@ -1,10 +1,8 @@
 import { writeFileSync } from "fs";
 const RSS = require("rss");
-import { HEADLINES } from "../data/jokes";
-import { sortByDate } from "../components/JokeParser/utilities/jokeUtils";
 import { RawJoke } from "../components/JokeParser/types";
 
-async function generate() {
+export async function generateFeed(jokes: RawJoke[]) {
   const feed = new RSS({
     title: "downtime.dev",
     description: "Hard-hitting tech news while your code compiles.",
@@ -12,31 +10,7 @@ async function generate() {
     feed_url: "https://www.downtime.dev/feed.xml",
   });
 
-  HEADLINES.forEach((headline) => {
-    feed.item({
-      title: headline.smoker
-        ? `${headline.smoker} ${headline.headline}`
-        : headline.headline,
-      description: `${feed.site_url}${headline.image}`,
-      url: `${feed.site_url}/#${headline.anchor}`,
-      date: headline.pubDate,
-    });
-  });
-
-  writeFileSync("./public/feed.xml", feed.xml({ indent: true }));
-}
-
-export async function generateFeed(values: RawJoke[]) {
-  const orderedJokes = sortByDate(values);
-
-  const feed = new RSS({
-    title: "downtime.dev",
-    description: "Hard-hitting tech news while your code compiles.",
-    site_url: "https://www.downtime.dev",
-    feed_url: "https://www.downtime.dev/feed.xml",
-  });
-
-  orderedJokes.forEach((headline) => {
+  jokes.forEach((headline) => {
     feed.item({
       title: headline.fields.smoker
         ? `${headline.fields.smoker} ${headline.fields.headline}`

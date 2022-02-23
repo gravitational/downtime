a5ccfcce5413579a00ef537e66cec4ccfe0712dd
Delia Constantino <delia.constantino@goteleport.com>
2022-02-23 15:19:07 -0500

    adjust spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,6 @@ import Logo from "components/Logo";
 import { createClient } from "contentful";
 import { generateFeed } from "../scripts/gen-rss";
 
-export interface HomeProps {
-  jokes: RawJoke[];
-}
-
-export default function Home({ jokes }: HomeProps) {
-  return (
-    <div className="flex flex-col items-center w-full">
-      <Logo />
-      <JokeParser jokes={jokes} />
-    </div>
-  );
-}
-
 export async function getStaticProps() {
   const contentfulClient = createClient({
     accessToken: `${process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN}`,
@@ -35,4 +22,17 @@ export async function getStaticProps() {
       revalidate: 1,
     },
   };
+}
+
+export interface HomeProps {
+  jokes: RawJoke[];
+}
+
+export default function Home({ jokes }: HomeProps) {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <Logo />
+      <JokeParser jokes={jokes} />
+    </div>
+  );
 }

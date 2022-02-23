5cf5a406a84e43d4c89950bfa3aad28f1f926b32
Delia Constantino <delia.constantino@goteleport.com>
2022-02-23 15:27:56 -0500

    fix isr

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,18 @@ import { JokeParser, RawJoke } from "components/JokeParser";
 import Logo from "components/Logo";
 import { createClient } from "contentful";
 import { generateFeed } from "../scripts/gen-rss";
+export interface HomeProps {
+  jokes: RawJoke[];
+}
+
+export default function Home({ jokes }: HomeProps) {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <Logo />
+      <JokeParser jokes={jokes} />
+    </div>
+  );
+}
 
 export async function getStaticProps() {
   const contentfulClient = createClient({
@@ -19,20 +31,7 @@ export async function getStaticProps() {
   return {
     props: {
       jokes: res.items,
-      revalidate: 1,
     },
+    revalidate: 1,
   };
-}
-
-export interface HomeProps {
-  jokes: RawJoke[];
-}
-
-export default function Home({ jokes }: HomeProps) {
-  return (
-    <div className="flex flex-col items-center w-full">
-      <Logo />
-      <JokeParser jokes={jokes} />
-    </div>
-  );
 }

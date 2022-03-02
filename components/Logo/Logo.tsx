c246bd329e72bd8267223f935b28f6e7dee2eab9
Delia Constantino <delia.constantino@goteleport.com>
2022-03-02 10:35:06 -0500

    move logo container to Logo

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -3,8 +3,10 @@ import logo from "../../public/assets/images/downtimeLogo.png";
 
 const Logo = () => {
   return (
-    <div className="w-6/12 max-w-[600px] mt-14 lg:mt-16">
-      <NextImage src={logo} alt="downtime logo" />
+    <div className="flex flex-col items-center w-full">
+      <div className="w-6/12 max-w-[600px] mt-14 lg:mt-16">
+        <NextImage src={logo} alt="downtime logo" />
+      </div>
     </div>
   );
 };

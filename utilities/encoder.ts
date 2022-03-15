70a15f858acff438654b7a92eb66de08c5153e34
Delia Constantino <delia.constantino@goteleport.com>
2022-03-15 10:48:19 -0400

    update preview url for tweet encoder

--- utilities/encoder.ts
+++ utilities/encoder.ts
@@ -1,14 +1,11 @@
 // for testing share on twitter links with Vercel preview
 const baseUrl =
-  "https://downtime-git-delia-ve-contentful-updates-gravitational.vercel.app";
+  "https://downtime-git-delia-updates-off-107-gravitational.vercel.app/";
 
-// const baseUrl = process.env.NEXT_PUBLIC_HOST || "https://www.downtime.dev";
+// const baseUrl = "https://www.downtime.dev";
 
 export const tweetEncoder = (headline: string, slug: string): string => {
   const url = `${baseUrl}/jokes/${slug}`;
-
-  // tweet summary-large-image twitter card only
-  // return `https://twitter.com/intent/tweet?text=${encodeURIComponent(url)}`
 
   // tweet headline + summary-large-image twitter card
   return `https://twitter.com/intent/tweet?text=${headline}%20${encodeURIComponent(

f4f98d8cd0f8aaaf26804eafe9911285f88adf27
Delia Constantino <delia.constantino@goteleport.com>
2022-03-17 11:52:03 -0400

    update env

--- utilities/getJokes.ts
+++ utilities/getJokes.ts
@@ -60,7 +60,7 @@ export default async function getJokes() {
 async function fetchJokesData() {
   // during development, pass string of target environment name (see ./configManager)
   // for production, getConfig() must be passed "master"
-  const config = getConfig("master");
+  const config = getConfig("test-env2");
 
   console.log("Fetching jokes data...");
 

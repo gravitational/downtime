8e9ecdf9c91a12e194c96a80898ecf1f6c5bde56
Delia Constantino <delia.constantino@goteleport.com>
2022-03-21 15:09:19 -0400

    add testing url

--- scripts/gen-rss.ts
+++ scripts/gen-rss.ts
@@ -6,8 +6,10 @@ export async function generateFeed(jokes: RawJoke[]) {
   const feed = new RSS({
     title: "downtime.dev",
     description: "Hard-hitting tech news while your code compiles.",
-    site_url: "https://www.downtime.dev",
-    feed_url: "https://www.downtime.dev/feed.xml",
+    // site_url: "https://www.downtime.dev",
+    site_url: "https://downtime-git-delia-update-rss-feed-gravitational.vercel.app/",
+    // feed_url: "https://www.downtime.dev/feed.xml",
+    feed_url: "https://downtime-git-delia-update-rss-feed-gravitational.vercel.app/feed.xml",
   });
 
   jokes.forEach((headline) => {
